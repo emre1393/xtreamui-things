1829ec37dcbb0717cbe303af8e03bba2d0919f1f
Emre K <9091762+emre1393@users.noreply.github.com>
2020-04-13 12:46:54 +0300

    new stuff

--- clearlogs.sh
+++ clearlogs.sh
@@ -19,3 +19,10 @@ sudo echo > /home/xtreamcodes/iptv_xtream_codes/logs/error.log
 #CREATE TABLE user_activity_new LIKE user_activity; RENAME TABLE user_activity TO user_activity_old, user_activity_new TO user_activity; DROP TABLE user_activity_old;
 
 #CREATE TABLE mag_logs_new LIKE mag_logs; RENAME TABLE mag_logs TO mag_logs_old, mag_logs_new TO mag_logs; DROP TABLE mag_logs_old;
+
+#DELETE FROM `panel_logs` WHERE `date` < UNIX_TIMESTAMP(NOW() - INTERVAL 10 DAY);  
+#DELETE FROM `user_activity` WHERE `date` < UNIX_TIMESTAMP(NOW() - INTERVAL 3 DAY);  
+#DELETE FROM `mag_logs` WHERE `date` < UNIX_TIMESTAMP(NOW() - INTERVAL 3 DAY);  
+#DELETE FROM `client_logs` WHERE `date` < UNIX_TIMESTAMP(NOW() - INTERVAL 3 DAY);  
+#DELETE FROM `stream_logs` WHERE `date` < UNIX_TIMESTAMP(NOW() - INTERVAL 1 DAY);  
+#DELETE FROM `epg_data` WHERE `start` < NOW() - INTERVAL 10 DAY;  
