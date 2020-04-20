a5138df8b8ca462c490bb1a78311a694b2fd06d7
Emre K <9091762+emre1393@users.noreply.github.com>
2020-04-20 17:37:53 +0300

    downloadbox iframe php files
    these files call downloadbox web interface pages in iframe.

---
+++ downloadbox-in-admin-panel/deluge.php
@@ -0,0 +1,57 @@
+<?php
+include "session.php"; include "functions.php";
+if (($rPermissions["is_admin"]) && (!hasPermissions("adv", "movies", "series"))) { exit; }
+$rStatusArray = Array(0 => "CLOSED", 1 => "OPEN", 2 => "RESPONDED", 3 => "READ");
+
+if ($rSettings["sidebar"]) {
+    include "header_sidebar.php";
+} else {
+    include "header.php";
+}
+        if ($rSettings["sidebar"]) { ?>
+        <div class="content-page"><div class="content"><div class="container-fluid">
+        <?php } else { ?>
+        <div class="wrapper"><div class="container-fluid">
+        <?php } ?>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box">
+                            <h4 class="page-title">Deluge for P2P Downloads</h4>
+<html lang="en">
+<!-- edit src="link" for your deluge web interface link -->
+    <center><iframe src="http://192.168.1.240:8112" style=" background: white; border: none; width: 100%; height: 800px; align: center"></iframe></center>
+</html>       
+
+                        </div>
+                    </div>
+
+                <!-- end row -->
+            </div> <!-- end container -->
+        </div>
+        <!-- end wrapper -->
+        <?php if ($rSettings["sidebar"]) { echo "</div>"; } ?>
+        <footer class="footer">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12 copyright text-center"><?=getFooter()?></div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer -->
+        <script src="assets/js/vendor.min.js"></script>
+        <script src="assets/libs/jquery-toast/jquery.toast.min.js"></script>
+        <script src="assets/libs/datatables/jquery.dataTables.min.js"></script>
+        <script src="assets/libs/datatables/dataTables.bootstrap4.js"></script>
+        <script src="assets/libs/datatables/dataTables.responsive.min.js"></script>
+        <script src="assets/libs/datatables/responsive.bootstrap4.min.js"></script>
+        <script src="assets/libs/datatables/dataTables.buttons.min.js"></script>
+        <script src="assets/libs/datatables/buttons.bootstrap4.min.js"></script>
+        <script src="assets/libs/datatables/buttons.html5.min.js"></script>
+        <script src="assets/libs/datatables/buttons.flash.min.js"></script>
+        <script src="assets/libs/datatables/buttons.print.min.js"></script>
+        <script src="assets/libs/datatables/dataTables.keyTable.min.js"></script>
+        <script src="assets/libs/datatables/dataTables.select.min.js"></script>
+        <script src="assets/libs/pdfmake/pdfmake.min.js"></script>
+        <script src="assets/libs/pdfmake/vfs_fonts.js"></script>
+    </body>
+</html>
