5dcf790d111ddf8eb4956ade6d5228ccc9ed13c2
Emre K <9091762+emre1393@users.noreply.github.com>
2020-04-25 19:16:14 +0300

    Create dlbox_links.php

---
+++ downloadbox-in-admin-panel/dlbox_links.php
@@ -0,0 +1,10 @@
+<?php
+$_ = Array(
+    "radarr_url" => "https://yourdomain.com/radarr",
+    "sonarr_url" => "https://yourdomain.com/sonarr",
+    "bazarr_url" => "https://yourdomain.com/bazarr",
+    "plex_url" => "https://yourdomain.com/plex",
+    "deluge_url" => "https://yourdomain.com/deluge",
+    "jackett_url" => "https://yourdomain.com/jackett"
+);
+?>
