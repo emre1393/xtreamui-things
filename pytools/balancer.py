953e1486424ea2c96acb422e7cdde1c057c686fb
Emre K <9091762+emre1393@users.noreply.github.com>
2020-08-25 19:08:44 +0300

    Update balancer.py
    put balancer.py from update.zip/pytools folder, previous one was for auto lb installation script.

--- pytools/balancer.py
+++ pytools/balancer.py
@@ -1,104 +1,77 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-import subprocess, os, sys
-from itertools import cycle, izip
+import paramiko, os, socket, time, json
+from config import decrypt
 
-rDownloadURL = "https://bitbucket.org/emre1393/xtreamui_mirror/downloads/sub_xtreamcodes_reborn.tar.gz"
-rPackages = ["libcurl3", "libxslt1-dev", "libgeoip-dev", "e2fsprogs", "wget", "mcrypt", "nscd", "htop", "zip", "unzip", "mc"]
+# Status: 0 - Not Started       1 - Started         2 - Done
 
-def getVersion():
-    try: return subprocess.check_output("lsb_release -d".split()).split(":")[-1].strip()
-    except: return ""
+def getIP():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    except: return None
 
-def prepare():
-    global rPackages
-    for rFile in ["/var/lib/dpkg/lock-frontend", "/var/cache/apt/archives/lock", "/var/lib/dpkg/lock"]:
-        try: os.remove(rFile)
-        except: pass
-    os.system("apt-get update > /dev/null")
-    os.system("apt-get remove --auto-remove libcurl4 -y > /dev/null")
-    for rPackage in rPackages: os.system("apt-get install %s -y > /dev/null" % rPackage)
-    os.system("wget -q -O /tmp/libpng12.deb http://mirrors.kernel.org/ubuntu/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1_amd64.deb")
-    os.system("dpkg -i /tmp/libpng12.deb > /dev/null")
-    os.system("apt-get install -y > /dev/null") # Clean up above
-    try: os.remove("/tmp/libpng12.deb")
+rDownloadURL = "https://github.com/emre1393/xtreamui_mirror/raw/master/balancer.py"
+rPath = "/home/xtreamcodes/iptv_xtream_codes/adtools/balancer/"
+rConfig = decrypt()
+rIP = getIP()
+rTime = time.time()
+
+def writeDetails(rDetails):
+    rFile = open("%s%d.json" % (rPath, int(rDetails["id"])), "w")
+    rFile.write(json.dumps(rDetails))
+    rFile.close()
+
+def installBalancer(rDetails):
+    rDetails["status"] = 1
+    writeDetails(rDetails)
+    rClient = paramiko.SSHClient()
+    rClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try: rClient.connect(rDetails["host"], rDetails["port"], "root", rDetails["password"])
+    except:
+        rDetails["status"] = 0
+        writeDetails(rDetails)
+        return True
+    try:
+        rIn, rOut, rErr = rClient.exec_command("sudo apt-get install python -y")
+        rStatus = rOut.channel.recv_exit_status()
+        rIn, rOut, rErr = rClient.exec_command("sudo wget -q \"%s\" -O \"/tmp/balancer.py\"" % rDownloadURL)
+        rStatus = rOut.channel.recv_exit_status()
+        rIn, rOut, rErr = rClient.exec_command("sudo python /tmp/balancer.py \"%s\" \"%s\" \"%s\" \"%s\" \"%s\" %d %d %d %d" % (rIP, rConfig["db_port"], rConfig["db_user"], rConfig["db_pass"], rConfig["db_name"], int(rDetails["id"]), int(rDetails["http_broadcast_port"]), int(rDetails["https_broadcast_port"]), int(rDetails["rtmp_port"])))
+        rStatus = rOut.channel.recv_exit_status()
     except: pass
-    os.system("adduser --system --shell /bin/false --group --disabled-login xtreamcodes > /dev/null")
-    if not os.path.exists("/home/xtreamcodes"): os.mkdir("/home/xtreamcodes")
+    rDetails["status"] = 2
+    try: os.remove("%s%d.json" % (rPath, int(rDetails["id"])))
+    except: writeDetails(rDetails)
     return True
 
-def install():
-    global rInstall, rDownloadURL
-    rURL = rDownloadURL
-    os.system('wget -q -O "/tmp/xtreamcodes.tar.gz" "%s"' % rURL)
-    if os.path.exists("/tmp/xtreamcodes.tar.gz"):
-        os.system('tar -zxvf "/tmp/xtreamcodes.tar.gz" -C "/home/xtreamcodes/" > /dev/null')
-        try: os.remove("/tmp/xtreamcodes.tar.gz")
-        except: pass
-        return True
-    return False
-
-def encrypt(rHost="127.0.0.1", rUsername="user_iptvpro", rPassword="", rDatabase="xtream_iptvpro", rServerID=1, rPort=7999):
-    try: os.remove("/home/xtreamcodes/iptv_xtream_codes/config")
+def restartServices(rDetails):
+    rClient = paramiko.SSHClient()
+    rClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try: rClient.connect(rDetails["host"], rDetails["port"], "root", rDetails["password"])
+    except: return False
+    rClient.exec_command("sudo /home/xtreamcodes/iptv_xtream_codes/start_services.sh")
+    try: os.remove("%s%d.json" % (rPath, int(rDetails["id"])))
     except: pass
-    rf = open('/home/xtreamcodes/iptv_xtream_codes/config', 'wb')
-    rf.write(''.join(chr(ord(c)^ord(k)) for c,k in izip('{\"host\":\"%s\",\"db_user\":\"%s\",\"db_pass\":\"%s\",\"db_name\":\"%s\",\"server_id\":\"%d\", \"db_port\":\"%d\"}' % (rHost, rUsername, rPassword, rDatabase, rServerID, rPort), cycle('5709650b0d7806074842c6de575025b1'))).encode('base64').replace('\n', ''))
-    rf.close()
+    return True
 
-def configure():
-    if not "/home/xtreamcodes/iptv_xtream_codes/" in open("/etc/fstab").read():
-        rFile = open("/etc/fstab", "a")
-        rFile.write("tmpfs /home/xtreamcodes/iptv_xtream_codes/streams tmpfs defaults,noatime,nosuid,nodev,noexec,mode=1777,size=90% 0 0\ntmpfs /home/xtreamcodes/iptv_xtream_codes/tmp tmpfs defaults,noatime,nosuid,nodev,noexec,mode=1777,size=2G 0 0")
-        rFile.close()
-    if not "xtreamcodes" in open("/etc/sudoers").read(): os.system('echo "xtreamcodes ALL = (root) NOPASSWD: /sbin/iptables" >> /etc/sudoers')
-    if not os.path.exists("/etc/init.d/xtreamcodes"):
-        rFile = open("/etc/init.d/xtreamcodes", "w")
-        rFile.write("#! /bin/bash\n/home/xtreamcodes/iptv_xtream_codes/start_services.sh")
-        rFile.close()
-        os.system("chmod +x /etc/init.d/xtreamcodes > /dev/null")
-    try: os.remove("/usr/bin/ffmpeg")
+def rebootServer(rDetails):
+    rClient = paramiko.SSHClient()
+    rClient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try: rClient.connect(rDetails["host"], rDetails["port"], "root", rDetails["password"])
+    except: return False
+    rClient.exec_command("sudo reboot")
+    try: os.remove("%s%d.json" % (rPath, int(rDetails["id"])))
     except: pass
-    os.system("ln -s /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg /usr/bin/")
-    try: os.mkdir("/home/xtreamcodes/iptv_xtream_codes/tv_archive")
-    except: pass
-    os.system("ln -s /home/xtreamcodes/iptv_xtream_codes/bin/ffmpeg /usr/bin/")
-    os.system("chattr -i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb > /dev/null")
-    os.system("wget -q https://bitbucket.org/emre1393/xtreamui_mirror/downloads/GeoLite2.mmdb -O /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb")
-    os.system("wget -q https://bitbucket.org/emre1393/xtreamui_mirror/downloads/pid_monitor.php -O /home/xtreamcodes/iptv_xtream_codes/crons/pid_monitor.php")
-    os.system("chown xtreamcodes:xtreamcodes -R /home/xtreamcodes > /dev/null")
-    os.system("chmod -R 0777 /home/xtreamcodes > /dev/null")
-    os.system("chattr +i /home/xtreamcodes/iptv_xtream_codes/GeoLite2.mmdb > /dev/null")
-    os.system("sed -i 's|chown -R xtreamcodes:xtreamcodes /home/xtreamcodes|chown -R xtreamcodes:xtreamcodes /home/xtreamcodes 2>/dev/null|g' /home/xtreamcodes/iptv_xtream_codes/start_services.sh")
-    os.system("chmod +x /home/xtreamcodes/iptv_xtream_codes/start_services.sh > /dev/null")
-    os.system("mount -a")
-    os.system("chmod 0700 /home/xtreamcodes/iptv_xtream_codes/config > /dev/null")
-    if not "api.xtream-codes.com" in open("/etc/hosts").read(): os.system('echo "127.0.0.1    api.xtream-codes.com" >> /etc/hosts')
-    if not "downloads.xtream-codes.com" in open("/etc/hosts").read(): os.system('echo "127.0.0.1    downloads.xtream-codes.com" >> /etc/hosts')
-    if not "xtream-codes.com" in open("/etc/hosts").read(): os.system('echo "127.0.0.1    xtream-codes.com" >> /etc/hosts')
-    if not "@reboot root /home/xtreamcodes/iptv_xtream_codes/start_services.sh" in open("/etc/crontab").read(): os.system('echo "@reboot root /home/xtreamcodes/iptv_xtream_codes/start_services.sh" >> /etc/crontab')
-
-def start(): os.system("/home/xtreamcodes/iptv_xtream_codes/start_services.sh > /dev/null")
-
-def setPorts(rPorts):
-    os.system("sed -i 's/listen 25461;/listen %d;/g' /home/xtreamcodes/iptv_xtream_codes/nginx/conf/nginx.conf" % rPorts[0])
-    os.system("sed -i 's/:25461/:%d/g' /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/conf/nginx.conf" % rPorts[0])
-    os.system("sed -i 's/listen 25463 ssl;/listen %d ssl;/g' /home/xtreamcodes/iptv_xtream_codes/nginx/conf/nginx.conf" % rPorts[1])
-    os.system("sed -i 's/listen 25462;/listen %d;/g' /home/xtreamcodes/iptv_xtream_codes/nginx_rtmp/conf/nginx.conf" % rPorts[2])
+    return True
 
 if __name__ == "__main__":
-    rHost = sys.argv[1]
-    rPort = int(sys.argv[2])
-    rUsername = sys.argv[3]
-    rPassword = sys.argv[4]
-    rDatabase = sys.argv[5]
-    rServerID = int(sys.argv[6])
-    try: rPorts = [int(sys.argv[7]), int(sys.argv[8]), int(sys.argv[9])]
-    except: rPorts = None
-    rRet = prepare()
-    if not install(): sys.exit(1)
-    encrypt(rHost, rUsername, rPassword, rDatabase, rServerID, rPort)
-    configure()
-    if rPorts: setPorts(rPorts)
-    start()
-    
-#13.03.2020 minimum R22B version is required for lb installation with custom ports.
+    if rIP and rConfig:
+        for rFile in os.listdir(rPath):
+            try: rDetails = json.loads(open(rPath + rFile).read())
+            except: rDetails = {"status": -1}
+            try: rType = rDetails["type"]
+            except: rType = None
+            if rType == "restart": restartServices(rDetails)
+            elif rType == "reboot": rebootServer(rDetails)
+            else:
+                if rDetails["status"] == 0: installBalancer(rDetails)
